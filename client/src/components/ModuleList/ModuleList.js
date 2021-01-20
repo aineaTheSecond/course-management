ad3bd58c1e00143ed4ada72053b6172f0b6f8796
Ainea Wabwoba <aineah28@gmail.com>
2021-01-20 12:27:31 +0300

    removed the module component to keep the code concise

--- client/src/components/ModuleList/ModuleList.js
+++ client/src/components/ModuleList/ModuleList.js
@@ -1,11 +1,50 @@
 import * as React from "react";
+import "./ModuleList.css";
 
-const ModuleList = (props) => {
+const ModuleList = () => {
   // fetch modules
+
+  const modules = [
+    {
+      module_name: "Introduction",
+      module_id: 1,
+      course_name: "Computer Science",
+    },
+    {
+      module_name: "Networking",
+      module_id: 2,
+      course_name: "Computer Science",
+    },
+  ];
 
   return (
     <div className="module-list container">
-      <h1>list of modules in course</h1>
+      <h1>list of modules in course: </h1>
+      <table className="module-table">
+        <thead>
+          <tr>
+            <th>Module</th>
+            <th>Course</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* render modules */}
+          {modules.map((module) => (
+            <tr key={module.module_id}>
+              <td>{module.module_name}</td>
+              <td>{module.course_name}</td>
+              <td>
+                <span class="material-icons module-icon">create</span>
+              </td>
+              <td>
+                <span class="material-icons module-icon">delete</span>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,16 +4,6 @@ import CourseList from "./CourseList/CourseList";
 import Nav from "./Main-Nav/Nav";
 import FormInput from "./FormInput/FormInput";
 import CourseForm from "./CourseForm/CourseForm";
-import Module from "./Module/Module";
 import ModuleList from "./ModuleList/ModuleList";
 
-export {
-  Header,
-  Course,
-  Nav,
-  CourseList,
-  FormInput,
-  CourseForm,
-  Module,
-  ModuleList,
-};
+export { Header, Course, Nav, CourseList, FormInput, CourseForm, ModuleList };
